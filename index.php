c8f1741a9f926ae9880c417a3cbe21d26e0e0d06
Olusola Adekunle <olusolaadek@yahoo.com>
2015-03-20 13:49:19 +0100

    After method foo created

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php
 
-    echo "Hello World! ". 'from Sola'; 
+    echo "Hello World! ". 'from Sola\n'; 
 
+    /**
+     * Summary of MyClass
+     
+     */
     class MyClass
     {
 	    var $p;
         var $q;
+        /**
+         * Summary of Foo
+         * @
+         */
         function Foo( )
         {
     	    echo __METHOD__;
             $this->q= new MyClass();
         }
+        
+        function Bar( )
+        {
+        	
+        }
+        
     
     }
+    // TODO: To implement this later
     $a = new MyClass;
+    $a->Foo(); 
+    echo '\n';
     $a->q->Foo();
 
 ?>
